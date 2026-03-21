c66a05e5c03529159883d0e69d8198810ab9cc67
Roy Strickland <roys@rdsventures.com>
2026-03-20 22:30:32 -0400

    Forece dashboard update

--- api/dashboard/index.js
+++ api/dashboard/index.js
@@ -5,15 +5,13 @@ module.exports = async function (context, req) {
       "Content-Type": "application/json"
     },
     body: {
-      customers: 124,
-      properties: 387,
-      openWorkOrders: 19,
+      customers: 123,
+      properties: 888,
+      openWorkOrders: 777,
+      newRequests: 666,
       recentActivity: [
-        "New customer added: ABC Property Management",
-        "Property imported: 1701 Running Brook Road",
-        "Work order #4821 marked complete"
+        "TEST DEPLOYMENT WORKED"
       ]
     }
   };
 };
-
