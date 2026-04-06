f618694470aa79067387bf9458a631d4c4265cbd
Roy Strickland <roys@rdsventures.com>
2026-04-06 17:30:38 -0400

    resetting project things areg getting messy

--- api/dashboardtest/index.js
+++ api/dashboardtest/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-  context.res = {
+  return {
     status: 200,
     headers: { "Content-Type": "application/json" },
     body: { ok: true, route: "dashboardtest" }
