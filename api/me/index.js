2d093abd5515959f81d16dd71dfe06f5db94b4de
Roy Strickland <roys@rdsventures.com>
2026-04-05 21:58:59 -0400

    adding time functionaility to app

--- api/me/index.js
+++ api/me/index.js
@@ -1,8 +1,19 @@
-const { getPool } = require("../shared/db");
-
 module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    body: { ok: true, hasGetPool: typeof getPool === "function" }
-  };
+  try {
+    context.res = {
+      status: 200,
+      body: {
+        ok: true,
+        message: "me function loaded"
+      }
+    };
+  } catch (err) {
+    context.res = {
+      status: 500,
+      body: {
+        error: err.message,
+        stack: err.stack
+      }
+    };
+  }
 };
