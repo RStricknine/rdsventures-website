6bde97b114d06131ea5ed76c791b5be6d9e0eac4
Roy Strickland <roys@rdsventures.com>
2026-04-06 17:05:46 -0400

    resetting project things areg getting messy

--- api/dashboard/index.js
+++ api/dashboard/index.js
@@ -1,21 +1,6 @@
 module.exports = async function (context, req) {
-  context.log("FUNCTION HIT");
-
-  try {
-    context.res = {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-      body: {
-        ok: true,
-        message: "function is running"
-      }
-    };
-  } catch (err) {
-    context.res = {
-      status: 500,
-      body: {
-        error: err.message
-      }
-    };
-  }
+  context.res = {
+    status: 200,
+    body: { ok: true }
+  };
 };
