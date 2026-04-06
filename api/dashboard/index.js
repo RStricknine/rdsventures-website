bf781010c6ee53fbbf50a9c3df38569b1016b384
Roy Strickland <roys@rdsventures.com>
2026-04-05 23:41:42 -0400

    resetting project things areg getting messy

--- api/dashboard/index.js
+++ api/dashboard/index.js
@@ -1,104 +1,12 @@
-const sql = require("mssql");
-
-let pool;
-
-async function getPool() {
-  if (pool) return pool;
-
-  pool = await sql.connect({
-    server: process.env.SQL_SERVER,
-    database: process.env.SQL_DATABASE,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    port: parseInt(process.env.SQL_PORT || "1433", 10),
-    options: {
-      encrypt: true,
-      trustServerCertificate: false
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      idleTimeoutMillis: 30000
-    }
-  });
-
-  return pool;
-}
-
 module.exports = async function (context, req) {
-  try {
-    const db = await getPool();
-
-   const countsResult = await db.request().query(`
-  SELECT
-      (SELECT COUNT(*) FROM dbo.Customers) AS Customers,
-      (SELECT COUNT(*) FROM dbo.Properties) AS Properties,
-      (SELECT COUNT(*)
-       FROM dbo.stg_WorkOrders
-       WHERE Status IN ('Warranty', 'Dispatched')) AS OpenWorkOrders,
-      (SELECT COUNT(*)
-       FROM dbo.stg_WorkOrders
-       WHERE Created >= DATEADD(DAY, -7, GETUTCDATE())) AS NewRequests,
-      (SELECT COUNT(*)
-       FROM dbo.ServiceRequests
-       WHERE RequestStatus = 'New'
-         AND IsDeleted = 0) AS NewServiceRequests;
-`);
-
-    const activityResult = await db.request().query(`
-      SELECT TOP 10
-          CONCAT(
-              'WO ',
-              ISNULL(WorkOrderNumber, CAST(RowID AS nvarchar(20))),
-              CASE
-                  WHEN Subject IS NOT NULL AND LTRIM(RTRIM(Subject)) <> ''
-                  THEN ' - ' + Subject
-                  ELSE ''
-              END,
-              CASE
-                  WHEN Address IS NOT NULL AND LTRIM(RTRIM(Address)) <> ''
-                  THEN ' @ ' + Address
-                  ELSE ''
-              END
-          ) AS Activity
-      FROM dbo.stg_WorkOrders
-      ORDER BY Created DESC, RowID DESC;
-    `);
-
-    const counts = countsResult.recordset[0] || {};
-
   context.res = {
-  status: 200,
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: {
-    customers: counts.Customers ?? 0,
-    properties: counts.Properties ?? 0,
-    openWorkOrders: counts.OpenWorkOrders ?? 0,
-    newRequests: counts.NewRequests ?? 0,
-    newServiceRequests: counts.NewServiceRequests ?? 0,
-    recentActivity: activityResult.recordset.map(r => r.Activity)
-  }
-};
-
-
-
-
-    
-  } catch (error) {
-    context.log.error("Dashboard API error:", error);
-
-    context.res = {
-      status: 500,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: {
-        error: "Failed to load dashboard data",
-        message: error.message,
-        code: error.code || null
-      }
-    };
-  }
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: {
+      ok: true,
+      message: "dashboard function is alive"
+    }
+  };
 };
