e3579ce27970d4cd5cb196c4e2678c6a9ce87b0f
Roy Strickland <roys@rdsventures.com>
2026-04-06 16:55:26 -0400

    resetting project things areg getting messy

--- api/dashboard/index.js
+++ api/dashboard/index.js
@@ -1,16 +1,21 @@
 module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-    body: {
-      ok: true,
-      env: {
-        SQL_SERVER: !!process.env.SQL_SERVER,
-        SQL_DATABASE: !!process.env.SQL_DATABASE,
-        SQL_USER: !!process.env.SQL_USER,
-        SQL_PASSWORD: !!process.env.SQL_PASSWORD,
-        SQL_PORT: process.env.SQL_PORT || null
+  context.log("FUNCTION HIT");
+
+  try {
+    context.res = {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+      body: {
+        ok: true,
+        message: "function is running"
       }
-    }
-  };
+    };
+  } catch (err) {
+    context.res = {
+      status: 500,
+      body: {
+        error: err.message
+      }
+    };
+  }
 };
