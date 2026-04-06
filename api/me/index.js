e6a58681568ca275fac44efea678901202b8f8a8
Roy Strickland <roys@rdsventures.com>
2026-04-05 21:50:53 -0400

    adding time functionaility to app

--- api/me/index.js
+++ api/me/index.js
@@ -1,20 +1,8 @@
-// inside /api/me/index.js temporarily
 const { getPool } = require("../shared/db");
 
 module.exports = async function (context, req) {
-  try {
-    const pool = await getPool();
-
-    const result = await pool.request().query("SELECT GETDATE() as Now");
-
-    context.res = {
-      status: 200,
-      body: result.recordset
-    };
-  } catch (err) {
-    context.res = {
-      status: 500,
-      body: err.message
-    };
-  }
+  context.res = {
+    status: 200,
+    body: { ok: true, hasGetPool: typeof getPool === "function" }
+  };
 };
