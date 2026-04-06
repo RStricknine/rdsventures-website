e6cee867b0064dd2ea4afb4270c1265ee071a4c8
Roy Strickland <roys@rdsventures.com>
2026-04-05 22:07:05 -0400

    adding time functionaility to app

--- api/me/index.js
+++ api/me/index.js
@@ -1,19 +1,12 @@
 module.exports = async function (context, req) {
-  try {
-    context.res = {
-      status: 200,
-      body: {
-        ok: true,
-        message: "me function loaded"
-      }
-    };
-  } catch (err) {
-    context.res = {
-      status: 500,
-      body: {
-        error: err.message,
-        stack: err.stack
-      }
-    };
-  }
+  context.res = {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: {
+      ok: true,
+      message: "me function loaded"
+    }
+  };
 };
