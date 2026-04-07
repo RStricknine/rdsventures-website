9c32a4cf0459b278a62e85abda582c2150e0da96
Roy Strickland <roys@rdsventures.com>
2026-04-06 20:40:07 -0400

    Testing after azure host fix

--- api/dashboardtest/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  return {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-    body: { ok: true, route: "dashboardtest" }
-  };
-};
